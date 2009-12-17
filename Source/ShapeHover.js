742aff46f786ad6321ae583a5006ff2a489ca9c5
Country <k1der.country@free.fr>
2009-12-17 02:43:29 -0700

    - updated comments

--- Source/ShapeHover.js
+++ Source/ShapeHover.js
@@ -1,19 +1,18 @@
 /*
 ---
-script: ShapeHover.js
-
 description: Add rollover effect on an image map
 
 license: MIT-style license.
 
-authors: Yannick Croissant
+authors:
+- Yannick Croissant
 
 inspiration:
-	Inspired by [jQuery Maphilight](http://davidlynch.org/js/maphilight/docs/) Copyright (c) 2008 [David Lynch](http://davidlynch.org), [MIT License](http://opensource.org/licenses/mit-license.php)
+- Inspired by [jQuery Maphilight](http://davidlynch.org/js/maphilight/docs/) Copyright (c) 2008 [David Lynch](http://davidlynch.org), [MIT License](http://opensource.org/licenses/mit-license.php)
 
 requires:
-	core/1.2.3: '*'
-	more/1.2.3.1: [Assets]
+- core/1.2.3: '*'
+- more/1.2.3.1: [Assets]
 
 provides: [ShapeHover]
 
